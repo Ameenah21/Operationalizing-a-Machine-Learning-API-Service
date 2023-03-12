3a9eb0f3ab23fc90fd3a3514d0367bf6887d0a1a
Ameenah21 <aminatlawal21@gmail.com>
2023-03-12 09:15:15 +0100

    edit run_kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -2,17 +2,18 @@
 
 # This tags and uploads an image to Docker Hub
 
-# Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=ameenah/housingmlapp
 
-# Step 2
+
 # Run the Docker Hub container with kubernetes
+kubectl run housingmlapp \
+	--image=$dockerpath:latest --port=80
 
 
-# Step 3:
 # List kubernetes pods
+kubectl get pods
 
-# Step 4:
 # Forward the container port to a host
+kubectl port-forward housingmlapp 8000:80
 
